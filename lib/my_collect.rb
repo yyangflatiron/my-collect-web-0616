9befc8493521188c7ce21506360b8ff9a2d0ce9b
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-25 11:08:21 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(names)
   i=0
   first_names = []
   while i<names.length
-    first_names << names[i].split.first.upcase
+    first_names << yield(names[i])
     i+=1
   end
   first_names
